0159aef2ba02f5d58f515f51fd247f053d52872c
Mohammad Sartaj Ansari <58524606+sartaj0@users.noreply.github.com>
2022-08-18 20:30:45 +0530

    Fifth commit

--- app/data_manager.py
+++ app/data_manager.py
@@ -1,10 +1,30 @@
 from typing import Union
+import sqlite3
 class MyPostDataBase(object):
 	def __init__(self):
 		self.posts = [
 		{"title": "Title 1", "content": "Content 1", "id": 1},
 		{"title": "Title 2", "content": "Content 2", "id": 2}
 		]
+		self.databasename = "./fastapi.db"
+		self.connection = sqlite3.connect(self.databasename)
+		cursor = self.connection.cursor()
+		command2 = """CREATE TABLE IF NOT EXISTS products(
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			name VARCHAR NOT NULL,
+			price INT NOT NULL,
+			is_sale BOOL DEFAULT 0,
+			inverntory INTEGER NOT NULL DEFAULT 0,
+			created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+		);"""
+		self.insert_command = 'INSERT INTO products(name, price, is_sale)VALUES(?, ?, ?);'
+		self.fetch_all_command = 'SELECT * FROM products'
+		cursor.execute(command2)
+		cursor.execute(self.insert_command, ('abc', 10, True))
+		output = cursor.execute(self.fetch_all_command)
+		print(output.fetchall())
+		self.connection.commit()
+
 
 	def store(self, data: dict):
 		self.posts.append(data)
@@ -31,5 +51,3 @@ class MyPostDataBase(object):
 				self.posts[i] = post
 				return True
 		return False
-
-	
--- app/main.py
+++ app/main.py
@@ -12,10 +12,10 @@ app = FastAPI()
 
 
 class Post(BaseModel):
-    title: str
-    content: str 
-    publication: bool = True
-    rating: Optional[int] = None
+	title: str
+	content: str 
+	publication: bool = True
+	rating: Optional[int] = None
 
 
 my_posts = MyPostDataBase()
@@ -23,58 +23,58 @@ my_posts = MyPostDataBase()
 
 @app.get("/")
 def root():
-    return {"message": "HelloWorld"}
+	return {"message": "HelloWorld"}
 
 
 @app.get("/posts")
 def get_posts():
-    return {'data': my_posts.posts}
+	return {'data': my_posts.posts}
 
 
 @app.post('/posts', status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
 
-    post_dict = post.dict()
-    post_dict["id"] = randrange(0, 1e+6)
-    my_posts.store(post_dict)
-    return {"data": post_dict}
+	post_dict = post.dict()
+	post_dict["id"] = randrange(0, 1e+6)
+	my_posts.store(post_dict)
+	return {"data": post_dict}
 
 
 @app.post("/temp")
 def temp_test(post: dict=Body(...)):
-    return {'message', 'temp tested Successfully'}
+	return {'message', 'temp tested Successfully'}
 
 
 @app.get("/posts/{idx}")
 def  get_post(idx: int, response: Response):
-    post_detail = my_posts.find_post(idx)
-    if post_detail is None:
-        # response.status_code = status.HTTP_404_NOT_FOUND
-        # return {"message": f'Post with id: {idx} was not found'}
-        ## OR
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {idx} was not found")
-    return {"post_detail":post_detail}
+	post_detail = my_posts.find_post(idx)
+	if post_detail is None:
+		# response.status_code = status.HTTP_404_NOT_FOUND
+		# return {"message": f'Post with id: {idx} was not found'}
+		## OR
+		raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {idx} was not found")
+	return {"post_detail":post_detail}
 
 
 ## We dont return data/message when we delete
 @app.delete("/posts/{idx}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(idx: int, response: Response):
-    execution = my_posts.delete_post(idx)
-    print(execution)
-    if execution:
-        return Response(status_code=status.HTTP_204_NO_CONTENT)
-    else:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {idx} was not found")
+	execution = my_posts.delete_post(idx)
+	print(execution)
+	if execution:
+		return Response(status_code=status.HTTP_204_NO_CONTENT)
+	else:
+		raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {idx} was not found")
 
 
 @app.put("/posts/{idx}")
 def update_post(idx: int, post: Post):
-    post_dict = post.dict()
-    post_dict["id"] = idx
-    execution = my_posts.update_post(idx, post_dict)
-    print(execution)
-    if execution:
-        return Response(status_code=status.HTTP_204_NO_CONTENT)
-    else:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {idx} was not found")
-    return {'message': "updated the post"}
+	post_dict = post.dict()
+	post_dict["id"] = idx
+	execution = my_posts.update_post(idx, post_dict)
+	print(execution)
+	if execution:
+		return Response(status_code=status.HTTP_204_NO_CONTENT)
+	else:
+		raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {idx} was not found")
+
