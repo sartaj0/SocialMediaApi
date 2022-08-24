9a40da9ef3f699b2ece6389ebc146006465b5c1d
Mohammad Sartaj <sartajansari9786@gmail.com>
2022-08-24 18:24:48 +0530

    added CORS

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from typing import List
 
 from fastapi import FastAPI
 from fastapi.params import Body
+from fastapi.middleware.cors import CORSMiddleware
 
 from . import models
 from .database import engine
@@ -12,7 +13,14 @@ from .config import settings
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.routers)
 app.include_router(user.routers)
